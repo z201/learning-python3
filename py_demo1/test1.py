e2b399bfdd87853cc6bf38067a6d96746574d3f8
z201 <z201_java_se@outlook.com>
2019-03-31 00:05:00 +0800

    添加python练习代码

---
+++ py_demo1/test1.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+print('如果需要输出中文，文件的第一行记得添加 # -*- coding: UTF-8 -*-')
+print('学习python的数据类型，已经基本的类型转换方法')
+print(8);
+print('8');
+print(8.8);
+print(True);
+print(False);
+print(bool(0));
+print(bool(1));
+print('在数字类型转换bool类型的时候，大于0的都是True')
+print(type(8));
+print(type('8'));
+print(type(8.8));
+print(type(True));
+print('字符串转换数字 int()');
+print(type(int('8')));
+print('数字转换字符串 str()');
+print(type(str(8)));
+print('数字转换bool值 bool()');
+print(type(bool(1)));
